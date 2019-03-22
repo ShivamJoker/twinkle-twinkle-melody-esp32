056b59db1d4217e5ec6a7fa35a5fd5ec5cc8525a
Shivam <shivam@MacBook-Pro-3.local>
2019-03-22 17:48:44 +0530

    commited

---
+++ twinkle-twinkle.ino
@@ -0,0 +1,195 @@
+// esp32 tone library by lbernstone
+// https://github.com/lbernstone/Tone
+#include <Tone32.h>
+
+
+#define BUZZER_PIN 14
+#define BUZZER_CHANNEL 0
+
+void setup() {}
+
+void loop()
+{
+    // twinkle twinkle little star
+
+    tone(BUZZER_PIN, NOTE_C4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_C4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_A4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_A4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(650);
+
+    //   How i wonder what you are
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_D4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_D4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_C4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(650);
+
+    // up above the world so high
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_D4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(650);
+
+    // like a diamond in the sky
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_D4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(650);
+
+    // twinkle twinkle little star
+
+    tone(BUZZER_PIN, NOTE_C4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_C4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_A4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_A4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_G4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(650);
+
+    //   How i wonder what you are
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_F4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_E4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_D4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_D4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(100);
+
+    tone(BUZZER_PIN, NOTE_C4, 650, BUZZER_CHANNEL);
+    noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    delay(650);
+    
+    // end of song
+    delay(5000);
+}
